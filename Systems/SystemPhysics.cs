5be8d6cf8c40fa94cbf255bcd3799190bdb4ab23
510666 <510666@2a431b5c-5de7-5447-8170-0889502f4b28>
2017-11-23 09:33:50 +0000

    Added the velocity ComponentType and the SystemPhysics.cs system.

---
+++ Systems/SystemPhysics.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK;
+using OpenGL_Game.Components;
+using OpenGL_Game.Objects;
+
+namespace OpenGL_Game.Systems
+{
+    class SystemPhysics : ISystem
+    {
+        const ComponentTypes MASK = (ComponentTypes.COMPONENT_POSITION | ComponentTypes.COMPONENT_VELOCITY);
+
+        public string Name
+        {
+            get { return "SystemPhysics"; }
+        }
+
+        public void OnAction(Entity entity)
+        {
+            if ((entity.Mask & MASK) == MASK)
+            {
+                List<IComponent> components = entity.Components;
+
+                IComponent positionComponent = components.Find(delegate (IComponent component)
+                {
+                    return component.ComponentType == ComponentTypes.COMPONENT_POSITION;
+                });
+
+                IComponent velocityComponent = components.Find(delegate (IComponent component)
+                {
+                    return component.ComponentType == ComponentTypes.COMPONENT_VELOCITY;
+                });
+
+                Motion((ComponentPosition)positionComponent, (ComponentVelocity)velocityComponent);
+            }
+        }
+
+        private void Motion(ComponentPosition pos, ComponentVelocity vel)
+        {
+            pos.Position += vel.Velocity * MyGame.dt;
+        }
+    }
+}
