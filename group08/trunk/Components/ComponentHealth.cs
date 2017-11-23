35d099819b57973ca5369c5688f4b310119a44fe
507974 <507974@2a431b5c-5de7-5447-8170-0889502f4b28>
2017-11-23 10:35:26 +0000

    Added Health Component - Cameron Owens

---
+++ group08/trunk/Components/ComponentHealth.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenGL_Game.Components
+{
+    class ComponentHealth : IComponent
+    {
+        int health;
+
+        public ComponentHealth(int h)
+        {
+            health = h;
+        }
+
+       
+        public int Health
+        {
+            get { return health; }
+            set { health = value; }
+        }
+
+        public ComponentTypes ComponentType
+        {
+            get { return ComponentTypes.COMPONENT_AMMO; }
+        }
+    }
+}
