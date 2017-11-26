a906171da6d65d3c07476c4a6701738af5c87b2c
507974 <507974@2a431b5c-5de7-5447-8170-0889502f4b28>
2017-11-26 22:48:58 +0000

    Components

---
+++ group08/trunk/Components/ComponentLight.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenGL_Game.Components
+{
+    class ComponentLight : IComponent
+    {
+        int intensity;
+     
+
+        public ComponentLight(int a)
+        {
+            intensity = a;
+        }
+
+       
+        public int Intensity
+        {
+            get { return intensity; }
+            set { intensity = value; }
+        }
+
+        public ComponentTypes ComponentType
+        {
+            get { return ComponentTypes.COMPONENT_LIGHT; }
+        }
+    }
+}
