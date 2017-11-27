a052320f5827024a87114e5ecbeff373eee25d37
514121 <514121@2a431b5c-5de7-5447-8170-0889502f4b28>
2017-11-27 08:05:22 +0000

    Movement added to SystemInput.cs.

--- Systems/SystemInput.cs
+++ Systems/SystemInput.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using OpenGL_Game.Objects;
 using OpenGL_Game.Components;
+using OpenTK.Input;
 
 namespace OpenGL_Game.Systems
 {
     class SystemInput : ISystem
     {
-        const ComponentTypes MASK = (ComponentTypes.COMPONENT_POSITION | ComponentTypes.COMPONENT_VELOCITY);
+        private KeyboardState KeyStates;
+        const ComponentTypes MASK = (ComponentTypes.COMPONENT_INPUT);
 
         public string Name
         {
@@ -18,6 +20,30 @@ namespace OpenGL_Game.Systems
 
         public void OnAction(Entity entity)
         {
+            if ((entity.Mask & MASK) == MASK)
+            {
+                KeyStates = Keyboard.GetState();
+
+                //Forward and Backwards motions
+                if (KeyStates.IsKeyDown(Key.W) || KeyStates.IsKeyDown(Key.Up))
+                {
+                    MyGame.gameInstance.playerCamera.ProcessMovement(Camera.CameraMovement.Forward);
+                }
+                else if (KeyStates.IsKeyDown(Key.S) || KeyStates.IsKeyDown(Key.Down))
+                {
+                    MyGame.gameInstance.playerCamera.ProcessMovement(Camera.CameraMovement.Backward);
+                }
+
+                //Left and Right motions
+                if (KeyStates.IsKeyDown(Key.A) || KeyStates.IsKeyDown(Key.Left))
+                {
+                    MyGame.gameInstance.playerCamera.ProcessMovement(Camera.CameraMovement.Left);
+                }
+                else if (KeyStates.IsKeyDown(Key.D) || KeyStates.IsKeyDown(Key.Right))
+                {
+                    MyGame.gameInstance.playerCamera.ProcessMovement(Camera.CameraMovement.Right);
+                }
+            }
         }
     }
 }
