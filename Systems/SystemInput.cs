c205ca9f6525e63fffb2ab9b78511904a437862e
514121 <514121@2a431b5c-5de7-5447-8170-0889502f4b28>
2017-11-30 09:10:18 +0000

    Camera can be moved side-to-side with mouse whilst right clicking.

--- Systems/SystemInput.cs
+++ Systems/SystemInput.cs
@@ -71,7 +71,10 @@ namespace OpenGL_Game.Systems
             float xOffset = MyGame.GetMousePosition().X - lastMousePosition.X;
             float yOffset = lastMousePosition.Y - MyGame.GetMousePosition().Y;
 
-            xOffset = 0; //Remove to allow side-to-side viewing
+            if (MyGame.GetMouse().GetCursorState().RightButton == ButtonState.Released)
+            {
+                xOffset = 0; //Disallow side-to-side viewing
+            }
 
             lastMousePosition.X = MyGame.GetMousePosition().X;
             lastMousePosition.Y = MyGame.GetMousePosition().Y;
