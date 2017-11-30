9d0e10e882bd57f2680a9315fa9e87f1b7c80448
510666 <510666@2a431b5c-5de7-5447-8170-0889502f4b28>
2017-11-30 04:40:06 +0000

    Moved the maze environment related functions to the bottom of the MyGame class, they will be moved in the future to a more suitable location.

--- MyGame.cs
+++ MyGame.cs
@@ -44,6 +44,208 @@ namespace OpenGL_Game
             systemManager = new SystemManager();
             AudioContext AC = new AudioContext();
         }
+       
+        private void CreateEntities()
+        {
+            Entity newEntity;
+
+            newEntity = new Entity("Player");
+            newEntity.AddComponent(new ComponentInput());
+            newEntity.AddComponent(new ComponentPosition(0.0f, 0.0f, 0.0f));
+            newEntity.AddComponent(new ComponentVelocity(0, 0, 0));
+            newEntity.AddComponent(new ComponentScale(0, 0, 0));
+            entityManager.AddEntity(newEntity);    
+            
+            setupMazeEnvironment();
+
+            for (int i = 0; i < wallPositions.Count; i++)
+            {
+                newEntity = new Entity("MazeWall");
+                newEntity.AddComponent(new ComponentVelocity(0, 0, 0.0f));
+
+                newEntity.AddComponent(new ComponentPosition(wallPositions[i]));
+                newEntity.AddComponent(new ComponentRotation(wallRotations[i]));
+                newEntity.AddComponent(new ComponentScale(wallScales[i])); 
+
+                newEntity.AddComponent(new ComponentGeometry("Geometry/QuadGeometry.txt"));
+                newEntity.AddComponent(new ComponentTexture("Textures/Oak.png"));
+                entityManager.AddEntity(newEntity);
+            }
+        }
+
+        private void CreateSystems()
+        {
+            ISystem newSystem;
+
+            newSystem = new SystemRender();
+            systemManager.AddSystem(newSystem);
+            newSystem = new SystemInput();
+            systemManager.AddSystem(newSystem);
+            newSystem = new SystemPhysics();
+            systemManager.AddSystem(newSystem);
+        }
+
+        /// <summary>
+        /// Allows the game to setup the environment and matrices.
+        /// </summary>
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            GL.ClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+            GL.Enable(EnableCap.DepthTest);
+            //GL.Enable(EnableCap.CullFace);
+
+            projection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(45), WIDTH / HEIGHT, 0.01f, 100f);
+
+            CreateEntities();
+            CreateSystems();
+
+            //LoadAudio();
+        }
+
+        private void LoadAudio()
+        {
+            // ********************************************************
+            // ***                    Audio                         ***
+            // ********************************************************
+
+            // Setup OpenAL Listener
+            listenerPosition = new Vector3(0, 0, 3);
+            listenerDirection = new Vector3(0, 0, -1);
+            listenerUp = Vector3.UnitY;
+
+            // reserve a Handle for the audio file
+            myBuffer = AL.GenBuffer();
+
+            // Load a .wav file from disk.
+            int channels, bits_per_sample, sample_rate;
+            byte[] sound_data = LoadWave(
+                File.Open("Audio/buzz.wav", FileMode.Open),
+                out channels,
+                out bits_per_sample,
+                out sample_rate);
+            ALFormat sound_format =
+                channels == 1 && bits_per_sample == 8 ? ALFormat.Mono8 :
+                channels == 1 && bits_per_sample == 16 ? ALFormat.Mono16 :
+                channels == 2 && bits_per_sample == 8 ? ALFormat.Stereo8 :
+                channels == 2 && bits_per_sample == 16 ? ALFormat.Stereo16 :
+                (ALFormat)0; // unknown
+            AL.BufferData(myBuffer, sound_format, sound_data, sound_data.Length, sample_rate);
+            if (AL.GetError() != ALError.NoError)
+            {
+                // respond to load error etc.
+            }
+
+            // Create a sounds source using the audio clip
+            mySource = AL.GenSource(); // gen a Source Handle
+            AL.Source(mySource, ALSourcei.Buffer, myBuffer); // attach the buffer to a source
+            AL.Source(mySource, ALSourceb.Looping, true); // source loops infinitely
+            emitterPosition = new Vector3(10.0f, 0.0f, 0.0f);
+            AL.Source(mySource, ALSource3f.Position, ref emitterPosition);
+            AL.SourcePlay(mySource);
+        }
+
+        /// <summary>
+        /// Load a WAV file.
+        /// </summary>
+        private static byte[] LoadWave(Stream stream, out int channels, out int bits, out int rate)
+        {
+            if (stream == null)
+                throw new ArgumentNullException("stream");
+
+            using (BinaryReader reader = new BinaryReader(stream))
+            {
+                // RIFF header
+                string signature = new string(reader.ReadChars(4));
+                if (signature != "RIFF")
+                    throw new NotSupportedException("Specified stream is not a wave file.");
+
+                int riff_chunck_size = reader.ReadInt32();
+
+                string format = new string(reader.ReadChars(4));
+                if (format != "WAVE")
+                    throw new NotSupportedException("Specified stream is not a wave file.");
+
+                // WAVE header
+                string format_signature = new string(reader.ReadChars(4));
+                if (format_signature != "fmt ")
+                    throw new NotSupportedException("Specified wave file is not supported.");
+
+                int format_chunk_size = reader.ReadInt32();
+                int audio_format = reader.ReadInt16();
+                int num_channels = reader.ReadInt16();
+                int sample_rate = reader.ReadInt32();
+                int byte_rate = reader.ReadInt32();
+                int block_align = reader.ReadInt16();
+                int bits_per_sample = reader.ReadInt16();
+
+                string data_signature = new string(reader.ReadChars(4));
+                if (data_signature != "data")
+                    throw new NotSupportedException("Specified wave file is not supported.");
+
+                int data_chunk_size = reader.ReadInt32();
+
+                channels = num_channels;
+                bits = bits_per_sample;
+                rate = sample_rate;
+
+                return reader.ReadBytes((int)reader.BaseStream.Length);
+            }
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="e">Provides a snapshot of timing values.</param>
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            // ------------------------ TIMING ------------------------
+            dt = (float)(e.Time);
+
+            // ------------------------ AUDIO ------------------------
+            // Move sounds source from right to left at 2.5 meters per second
+            emitterPosition = new Vector3(emitterPosition.X - (float)(2.5 * e.Time), emitterPosition.Y, emitterPosition.Z);
+            AL.Source(mySource, ALSource3f.Position, ref emitterPosition);
+
+            // update OpenAL
+            AL.Listener(ALListener3f.Position, ref listenerPosition);
+            AL.Listener(ALListenerfv.Orientation, ref listenerDirection, ref listenerUp);
+
+            // TODO: Add your update logic here
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="e">Provides a snapshot of timing values.</param>
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            GL.Viewport(0, 0, Width, Height);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            systemManager.ActionSystems(entityManager);
+
+            GL.Flush();
+            SwapBuffers();
+        }
+
+        /// <summary>
+        /// Mouse is contained inside the GameWindow class.
+        /// </summary>
+        public static Vector2 GetMousePosition()
+        {
+            return new Vector2(gameInstance.Mouse.X, gameInstance.Mouse.Y);
+        }
+
+        public static void ExitGame()
+        {
+            gameInstance.Exit();
+        }
 
         List<Vector3> wallPositions = new List<Vector3>();
         List<Vector3> wallRotations = new List<Vector3>();
@@ -316,7 +518,7 @@ namespace OpenGL_Game
         /// Sets up all the information used to create the wall entities.
         /// </summary>
         void setupMazeEnvironment()
-        {    
+        {
             // Maze walls
             addWallPositions();
             addWallRotations();
@@ -339,209 +541,6 @@ namespace OpenGL_Game
             wallScales.Add(new Vector3(12.5f, 0.0f, 12.5f));
 
             addOuterWalls();
-        }
-        
-
-        private void CreateEntities()
-        {
-            Entity newEntity;
-
-            newEntity = new Entity("Player");
-            newEntity.AddComponent(new ComponentInput());
-            newEntity.AddComponent(new ComponentPosition(0.0f, 0.0f, 0.0f));
-            newEntity.AddComponent(new ComponentVelocity(0, 0, 0));
-            newEntity.AddComponent(new ComponentScale(0, 0, 0));
-            entityManager.AddEntity(newEntity);    
-            
-            setupMazeEnvironment();
-
-            for (int i = 0; i < wallPositions.Count; i++)
-            {
-                newEntity = new Entity("MazeWall");
-                newEntity.AddComponent(new ComponentVelocity(0, 0, 0.0f));
-
-                newEntity.AddComponent(new ComponentPosition(wallPositions[i]));
-                newEntity.AddComponent(new ComponentRotation(wallRotations[i]));
-                newEntity.AddComponent(new ComponentScale(wallScales[i])); 
-
-                newEntity.AddComponent(new ComponentGeometry("Geometry/QuadGeometry.txt"));
-                newEntity.AddComponent(new ComponentTexture("Textures/Oak.png"));
-                entityManager.AddEntity(newEntity);
-            }
-        }
-
-        private void CreateSystems()
-        {
-            ISystem newSystem;
-
-            newSystem = new SystemRender();
-            systemManager.AddSystem(newSystem);
-            newSystem = new SystemInput();
-            systemManager.AddSystem(newSystem);
-            newSystem = new SystemPhysics();
-            systemManager.AddSystem(newSystem);
-        }
-
-        /// <summary>
-        /// Allows the game to setup the environment and matrices.
-        /// </summary>
-        protected override void OnLoad(EventArgs e)
-        {
-            base.OnLoad(e);
-            GL.ClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-            GL.Enable(EnableCap.DepthTest);
-            //GL.Enable(EnableCap.CullFace);
-
-            projection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(45), WIDTH / HEIGHT, 0.01f, 100f);
-
-            CreateEntities();
-            CreateSystems();
-
-            //LoadAudio();
-        }
-
-        private void LoadAudio()
-        {
-            // ********************************************************
-            // ***                    Audio                         ***
-            // ********************************************************
-
-            // Setup OpenAL Listener
-            listenerPosition = new Vector3(0, 0, 3);
-            listenerDirection = new Vector3(0, 0, -1);
-            listenerUp = Vector3.UnitY;
-
-            // reserve a Handle for the audio file
-            myBuffer = AL.GenBuffer();
-
-            // Load a .wav file from disk.
-            int channels, bits_per_sample, sample_rate;
-            byte[] sound_data = LoadWave(
-                File.Open("Audio/buzz.wav", FileMode.Open),
-                out channels,
-                out bits_per_sample,
-                out sample_rate);
-            ALFormat sound_format =
-                channels == 1 && bits_per_sample == 8 ? ALFormat.Mono8 :
-                channels == 1 && bits_per_sample == 16 ? ALFormat.Mono16 :
-                channels == 2 && bits_per_sample == 8 ? ALFormat.Stereo8 :
-                channels == 2 && bits_per_sample == 16 ? ALFormat.Stereo16 :
-                (ALFormat)0; // unknown
-            AL.BufferData(myBuffer, sound_format, sound_data, sound_data.Length, sample_rate);
-            if (AL.GetError() != ALError.NoError)
-            {
-                // respond to load error etc.
-            }
-
-            // Create a sounds source using the audio clip
-            mySource = AL.GenSource(); // gen a Source Handle
-            AL.Source(mySource, ALSourcei.Buffer, myBuffer); // attach the buffer to a source
-            AL.Source(mySource, ALSourceb.Looping, true); // source loops infinitely
-            emitterPosition = new Vector3(10.0f, 0.0f, 0.0f);
-            AL.Source(mySource, ALSource3f.Position, ref emitterPosition);
-            AL.SourcePlay(mySource);
-        }
-
-        /// <summary>
-        /// Load a WAV file.
-        /// </summary>
-        private static byte[] LoadWave(Stream stream, out int channels, out int bits, out int rate)
-        {
-            if (stream == null)
-                throw new ArgumentNullException("stream");
-
-            using (BinaryReader reader = new BinaryReader(stream))
-            {
-                // RIFF header
-                string signature = new string(reader.ReadChars(4));
-                if (signature != "RIFF")
-                    throw new NotSupportedException("Specified stream is not a wave file.");
-
-                int riff_chunck_size = reader.ReadInt32();
-
-                string format = new string(reader.ReadChars(4));
-                if (format != "WAVE")
-                    throw new NotSupportedException("Specified stream is not a wave file.");
-
-                // WAVE header
-                string format_signature = new string(reader.ReadChars(4));
-                if (format_signature != "fmt ")
-                    throw new NotSupportedException("Specified wave file is not supported.");
-
-                int format_chunk_size = reader.ReadInt32();
-                int audio_format = reader.ReadInt16();
-                int num_channels = reader.ReadInt16();
-                int sample_rate = reader.ReadInt32();
-                int byte_rate = reader.ReadInt32();
-                int block_align = reader.ReadInt16();
-                int bits_per_sample = reader.ReadInt16();
-
-                string data_signature = new string(reader.ReadChars(4));
-                if (data_signature != "data")
-                    throw new NotSupportedException("Specified wave file is not supported.");
-
-                int data_chunk_size = reader.ReadInt32();
-
-                channels = num_channels;
-                bits = bits_per_sample;
-                rate = sample_rate;
-
-                return reader.ReadBytes((int)reader.BaseStream.Length);
-            }
-        }
-
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="e">Provides a snapshot of timing values.</param>
-        protected override void OnUpdateFrame(FrameEventArgs e)
-        {
-            base.OnUpdateFrame(e);
-
-            // ------------------------ TIMING ------------------------
-            dt = (float)(e.Time);
-
-            // ------------------------ AUDIO ------------------------
-            // Move sounds source from right to left at 2.5 meters per second
-            emitterPosition = new Vector3(emitterPosition.X - (float)(2.5 * e.Time), emitterPosition.Y, emitterPosition.Z);
-            AL.Source(mySource, ALSource3f.Position, ref emitterPosition);
-
-            // update OpenAL
-            AL.Listener(ALListener3f.Position, ref listenerPosition);
-            AL.Listener(ALListenerfv.Orientation, ref listenerDirection, ref listenerUp);
-
-            // TODO: Add your update logic here
-        }
-
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="e">Provides a snapshot of timing values.</param>
-        protected override void OnRenderFrame(FrameEventArgs e)
-        {
-            base.OnRenderFrame(e);
-
-            GL.Viewport(0, 0, Width, Height);
-            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-
-            systemManager.ActionSystems(entityManager);
-
-            GL.Flush();
-            SwapBuffers();
-        }
-
-        /// <summary>
-        /// Mouse is contained inside the GameWindow class.
-        /// </summary>
-        public static Vector2 GetMousePosition()
-        {
-            return new Vector2(gameInstance.Mouse.X, gameInstance.Mouse.Y);
-        }
-
-        public static void ExitGame()
-        {
-            gameInstance.Exit();
         }
     }
 }
