3f7e751e0cae8efb03bce4322513e4e1495f65f3
514121 <514121@2a431b5c-5de7-5447-8170-0889502f4b28>
2017-12-07 10:36:40 +0000

    Half completed audio system.

--- group08/trunk/Managers/ResourceManager.cs
+++ group08/trunk/Managers/ResourceManager.cs
@@ -7,6 +7,10 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using OpenTK.Graphics.OpenGL;
 using OpenGL_Game.Objects;
+using System.IO;
+using OpenTK.Audio.OpenAL;
+using OpenTK;
+using OpenGL_Game.Components;
 
 namespace OpenGL_Game.Managers
 {
@@ -14,6 +18,7 @@ namespace OpenGL_Game.Managers
     {
         static Dictionary<string, Geometry> geometryDictionary = new Dictionary<string, Geometry>();
         static Dictionary<string, int> textureDictionary = new Dictionary<string, int>();
+        static Dictionary<string, int> soundDictionary = new Dictionary<string, int>();
 
         public static Geometry LoadGeometry(string filename)
         {
@@ -58,6 +63,109 @@ namespace OpenGL_Game.Managers
             }
  
             return texture;
+        }
+
+
+        internal static byte[] LoadWave(FileStream fileStream, out int channels, out int bits, out int rate, out int chunkSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        //This is used for playing an individual sound, not for beginning of the project
+        public static ASound LoadAudio(Vector3 emitterPosition, string soundName)
+        {
+            // Setup OpenAL Listener - each component listener has these now
+            //listenerPosition = new Vector3(0, 0, 3);
+            //listenerDirection = new Vector3(0, 0, -1);
+            //listenerUp = Vector3.UnitY;
+
+            // reserve a Handle for the audio file
+            int myBuffer = AL.GenBuffer();
+
+            // Load a .wav file from disk.
+            int channels, bits_per_sample, sample_rate;
+            byte[] sound_data = LoadWave(
+                File.Open("Audio/" + soundName + ".wav", FileMode.Open),
+                out channels,
+                out bits_per_sample,
+                out sample_rate);
+            ALFormat sound_format =
+                channels == 1 && bits_per_sample == 8 ? ALFormat.Mono8 :
+                channels == 1 && bits_per_sample == 16 ? ALFormat.Mono16 :
+                channels == 2 && bits_per_sample == 8 ? ALFormat.Stereo8 :
+                channels == 2 && bits_per_sample == 16 ? ALFormat.Stereo16 :
+                (ALFormat)0; // unknown
+            AL.BufferData(myBuffer, sound_format, sound_data, sound_data.Length, sample_rate);
+            if (AL.GetError() != ALError.NoError)
+            {
+                // respond to load error etc.
+            }
+
+            // Create a sounds source using the audio clip
+            int mySource = AL.GenSource(); // gen a Source Handle
+
+            return new ASound(myBuffer, mySource, sound_data, emitterPosition);
+        }
+
+        /// <summary>
+        /// Load a WAV file.
+        /// </summary>
+        public static byte[] LoadWave(Stream stream, out int channels, out int bits, out int rate)
+        {
+            if (stream == null)
+                throw new ArgumentNullException("stream");
+
+            using (BinaryReader reader = new BinaryReader(stream))
+            {
+                // RIFF header
+                string signature = new string(reader.ReadChars(4));
+                if (signature != "RIFF")
+                    throw new NotSupportedException("Specified stream is not a wave file.");
+
+                int riff_chunck_size = reader.ReadInt32();
+
+                string format = new string(reader.ReadChars(4));
+                if (format != "WAVE")
+                    throw new NotSupportedException("Specified stream is not a wave file.");
+
+                // WAVE header
+                string format_signature = new string(reader.ReadChars(4));
+                if (format_signature != "fmt ")
+                    throw new NotSupportedException("Specified wave file is not supported.");
+
+                int format_chunk_size = reader.ReadInt32();
+                int audio_format = reader.ReadInt16();
+                int num_channels = reader.ReadInt16();
+                int sample_rate = reader.ReadInt32();
+                int byte_rate = reader.ReadInt32();
+                int block_align = reader.ReadInt16();
+                int bits_per_sample = reader.ReadInt16();
+
+                string data_signature = new string(reader.ReadChars(4));
+                if (data_signature != "data")
+                    throw new NotSupportedException("Specified wave file is not supported.");
+
+                int data_chunk_size = reader.ReadInt32();
+
+                channels = num_channels;
+                bits = bits_per_sample;
+                rate = sample_rate;
+
+                return reader.ReadBytes((int)reader.BaseStream.Length);
+            }
+
+        }
+        public static ALFormat GetSoundFormat(int channels, int bits)
+        {
+            switch (channels)
+            {
+                case 1:
+                    return bits == 8 ? ALFormat.Mono8 : ALFormat.Mono16;
+                case 2:
+                    return bits == 8 ? ALFormat.Stereo8 : ALFormat.Stereo16;
+                default:
+                    throw new NotSupportedException("The specified sound format is not supported.");
+            }
         }
     }
 }
